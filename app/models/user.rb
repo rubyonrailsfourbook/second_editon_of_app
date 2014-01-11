4049f1a93cef778c0cdfc6799df9e010da3d53df
Matthew Burket <matthew@assignitapp.com>
2014-01-10 21:51:40 -0600

    Improved user vaildtions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	
-	validates_presence_of :name, :email, :password
+	validates_presence_of :name, :email
+
+	validates_presence_of :password, on: :create
+
+	validates_length_of :email, :in => 8..512
+
+	validates :email, email: true
 
 	has_many :articles
 end
